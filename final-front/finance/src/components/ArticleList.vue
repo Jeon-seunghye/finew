f5541bda7afce154c1bb4b8d76ecd78e8a88a480
Seunghye <jeonsh1001@hanyang.ac.kr>
2023-11-19 21:49:36 +0900

    PostDetailView_add

--- final-front/finance/src/components/ArticleList.vue
+++ final-front/finance/src/components/ArticleList.vue
@@ -8,7 +8,9 @@
         <h3>{{ article.id }}번 게시글</h3>
         <p>작성자 : {{ article.user.username }}</p>
         <p>제목 : {{ article.title }}</p>
-        <p>내용 : {{ article.content }}</p>
+        <router-link :to="{ name: 'postdetail', params: { id: article.id } }">
+          <button class="view-detail-button">게시글 상세 보기</button>
+        </router-link>
         <hr>
       </div>
     </ul>
@@ -26,5 +28,18 @@ onMounted(() => {
 </script>
 
 <style scoped>
+.view-detail-button {
+  font-family: 'Noto Sans KR', sans-serif;
+  padding: 8px;
+  background-color: #3498db; /* 파란색 */
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
 
+.view-detail-button:hover {
+  background-color: #2980b9; /* 더 어두운 파란색 */
+}
 </style>
--- final-front/finance/src/views/PostView.vue
+++ final-front/finance/src/views/PostView.vue
@@ -1,7 +1,8 @@
+
 <template>
   <div>
-    <h1>게시글 목록 페이지</h1>
-    <button @click="router.push({name: 'create'})">게시글 생성</button>
+    <h1 class="board-title">Board</h1>
+    <button @click="router.push({ name: 'create' })" class="create-button">새 글 쓰기</button>
     <ArticleList />
   </div>
 </template>
@@ -13,5 +14,53 @@ const router = useRouter()
 </script>
 
 <style scoped>
+.board-title {
+  font-family: 'Noto Sans KR', sans-serif; /* Noto Sans KR 글씨체 적용 */
+  font-size: 28px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  text-align: center;
+  color: #3498db; /* 파란색 */
+}
 
+.create-button {
+  font-family: 'Noto Sans KR', sans-serif; /* Noto Sans KR 글씨체 적용 */
+  padding: 12px;
+  background-color: #2980b9; /* 짙은 파란색 */
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  display: block; /* 버튼을 블록 레벨로 표시하여 가로 중앙 정렬 */
+  margin: 0 auto; /* 가로 중앙 정렬 */
+}
+
+.create-button:hover {
+  background-color: #1f618d; /* 더 어두운 파란색 */
+}
+
+/* 게시글 목록 스타일링 */
+.article-list {
+  list-style: none;
+  padding: 0;
+}
+
+.article-item {
+  background-color: #ecf0f1; /* 연한 회색 */
+  border: 1px solid #bdc3c7; /* 연한 은색 */
+  border-radius: 5px;
+  margin-bottom: 10px;
+  padding: 15px;
+}
+
+.article-title {
+  font-size: 18px;
+  color: #2c3e50; /* 어두운 회색 */
+}
+
+.article-content {
+  margin-top: 10px;
+  color: #34495e; /* 더 어두운 회색 */
+}
 </style>
