fccc3a2c8a2daf288581407d6a9103273cc3a7d1
Jeon-seunghye <jfn022@gmail.com>
2023-11-20 17:48:08 +0900

    ArticleView_MapView

--- final-front/finance/src/views/MapView.vue
+++ final-front/finance/src/views/MapView.vue
@@ -1,24 +1,106 @@
 <template>
-  <main>
-    <h1>근처 은행</h1>
-    <div class="map" id="map"></div>
-  </main>
+  <div>
+    <select v-model="selectedDistrict" @change="searchBanksByDistrict">
+      <option v-for="district in seoulDistricts" :key="district.name" :value="district.name">
+        {{ district.name }}
+      </option>
+    </select>
+    <div ref="map" style="width: 100%; height: 500px;"></div>
+  </div>
 </template>
 
-<!-- <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=8d50621f2057740f292ad9da94105701">
-  const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-  let options = { //지도를 생성할 때 필요한 기본 옵션
-    center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-    level: 3 //지도의 레벨(확대, 축소 정도)
-  };
-  let map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-</script> -->
+
 <script>
+  import { ref, onMounted } from "vue";
+
+  export default {
+    setup() {
+      const map = ref(null);
+      const selectedDistrict = ref("종로구");
+      const markers = ref([]);
+
+      const seoulDistricts = [
+        { name: "종로구", lat: 37.572036, lng: 126.976594 },
+        { name: "중구", lat: 37.564613, lng: 126.997596 },
+        { name: "용산구", lat: 37.531100, lng: 126.981074 },
+        // ... (다른 구의 위도 경도 추가)
+      ];
+
+      onMounted(() => {
+        const script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=8d50621f2057740f292ad9da94105701&libraries=services&autoload=false`;
+        script.async = true;
+        script.onload = () => initializeMap(map);
+        document.head.appendChild(script);
+      });
+
+      const initializeMap = (mapRef) => {
+        window.kakao.maps.load(() => {
+          const container = mapRef.value;
+          const options = {
+            center: new window.kakao.maps.LatLng(37.5665, 126.9780),
+            level: 5,
+          };
+
+          const kakaoMap = new window.kakao.maps.Map(container, options);
+
+          // 초기화 시에도 선택한 구에 대한 은행을 검색하고 해당 위치로 이동
+          searchBanks(selectedDistrict.value, kakaoMap);
+        });
+      };
+
+      const searchBanksByDistrict = () => {
+        const district = selectedDistrict.value;
+        searchBanks(district, map.value);
+      };
+
+      const searchBanks = (gu, map) => {
+        const ps = new window.kakao.maps.services.Places();
+        ps.keywordSearch(`은행 ${gu}`, (data, status, pagination) => {
+          if (status === window.kakao.maps.services.Status.OK) {
+            clearMarkers();
+            for (let i = 0; i < data.length; i++) {
+              displayMarker(data[i], map);
+            }
+
+            // 검색된 은행이 있을 경우, 첫 번째 은행 위치로 이동
+            if (data.length > 0) {
+              const firstBank = data[0];
+              map.panTo(new window.kakao.maps.LatLng(firstBank.y, firstBank.x));
+            }
+          }
+        });
+      };
+
+      const displayMarker = (place, map) => {
+        const marker = new window.kakao.maps.Marker({
+          map: map,
+          position: new window.kakao.maps.LatLng(place.y, place.x),
+        });
+
+        window.kakao.maps.event.addListener(marker, "click", () => {
+          console.log(place);
+        });
+
+        markers.value.push(marker);
+      };
+
+      const clearMarkers = () => {
+        markers.value.forEach((marker) => {
+          marker.setMap(null);
+        });
+        markers.value = [];
+      };
+
+      return { map, selectedDistrict, seoulDistricts, searchBanksByDistrict };
+    },
+  };
 </script>
 
 <style scoped>
-.map{
-  width: 500px;
-  height: 400px;
+#map {
+  width: 90%;
+  height: 500px;
 }
 </style>
--- final-front/finance/src/views/ProfileView.vue
+++ final-front/finance/src/views/ProfileView.vue
@@ -1,11 +1,116 @@
-<template>
+<!-- <template>
   <main>
     <h1>프로필</h1>
+    <p>나이 : {{ store.data }}</p>
+    <button @click="updateProfile" class="delete-button">프로필 수정</button>
   </main>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+import { useArticleStore } from '@/stores/article';
+
+const store = useArticleStore();
+const user = ref({})
+const updateProfile = function (payload) {
+  const { username, email, age, money, salary, nickname } = payload;
+  axios({
+    method: 'put',  // Assuming you want to use the PUT method for updating the profile
+    url: `${store.API_URL}/accounts/profile/`,
+    data: {
+      username,
+      email,
+      age,
+      money,
+      salary,
+      nickname,
+    },
+    headers: {
+      Authorization: `Token ${token.value}`,
+    },
+  })
+    .then((response) => {
+      console.log(response.data);
+      // Update the local token if necessary
+      token.value = response.data.key;
+      // Redirect to the home page or another appropriate route
+      router.push({ name: 'home' });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+
 </script>
 
 <style scoped>
+</style> -->
+
+<template>
+  <main>
+    <h1>프로필</h1>
+
+    <!-- 사용자 정보 출력 -->
+    <p>이름: {{ user.username }}</p>
+    <p>나이: {{ user.age }}</p>
+    <p>이메일: {{ user.email }}</p>
+    <!-- 기타 필요한 사용자 정보를 출력 -->
+
+    <!-- 프로필 수정 버튼 -->
+    <button @click="toggleProfileForm" class="delete-button">프로필 수정</button>
+
+    <!-- 프로필 수정 폼 -->
+    <form v-if="isProfileFormVisible" @submit.prevent="submitProfileForm">
+      <label for="newUsername">새로운 사용자 이름:</label>
+      <input id="newUsername" v-model="newUsername" />
+      <label for="newAge">새로운 나이:</label>
+      <input id="newAge" type="number" v-model="newAge" />
+      <!-- 기타 필요한 수정할 필드들을 추가 -->
+      <button type="submit">저장</button>
+    </form>
+  </main>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useArticleStore } from '@/stores/article';
+
+const store = useArticleStore();
+const isProfileFormVisible = ref(false);
+const newUsername = ref('');
+const newAge = ref('');
+
+const user = ref({}); // Assuming you have a user object in the store
+
+// 프로필 수정 버튼 클릭 시 폼 토글
+const toggleProfileForm = () => {
+  isProfileFormVisible.value = !isProfileFormVisible.value;
+};
+
+// 프로필 수정 폼 제출 시 호출되는 함수
+const submitProfileForm = () => {
+  // 간단한 유효성 검사
+  if (newUsername.trim() === '' || newAge === '') {
+    alert('새로운 사용자 이름과 나이를 입력하세요.');
+    return;
+  }
+
+  // 서버에 사용자 정보 업데이트 요청
+  store.updateProfile({
+    username: newUsername,
+    age: newAge,
+    // 다른 필요한 필드들을 추가로 전달할 수 있습니다.
+  });
+
+  // 폼 초기화
+  newUsername.value = '';
+  newAge.value = '';
+  // 폼 숨기기
+  isProfileFormVisible.value = false;
+};
+</script>
+
+<style scoped>
+/* 추가적인 스타일을 필요에 따라 추가하세요 */
 </style>
