d339ebe49095258196239aebd6c5aa76b4e108bc
jaehunbb <sirika2547@naver.com>
2023-11-22 19:35:37 +0900

    deposit Btn finish

--- final-front/finance/src/views/DepositDetailView.vue
+++ final-front/finance/src/views/DepositDetailView.vue
@@ -17,7 +17,6 @@
       <p>저축 기간 : {{ option.save_trm }}개월</p>
       <p>저축 금리 : {{ option.intr_rate }}[%]</p>
       <p>최고 우대금리 : {{ option.intr_rate2 }}[%]</p>
-
       <button @click="store.addCart(option.id)">
         {{ isAdded(option.id) ? '가입취소' : '가입하기' }}
       </button>
@@ -39,6 +38,16 @@
   
   const fin_prdt_cd = ref(route.params.fin_prdt_cd)
 
+
+  // 상품코드랑 같은 정보 갖고오기
+  const selectedDeposit = ref({})
+  const getSelectedDeposit = function () {
+    const selectedThing = store.deposits.find(data => data.fin_prdt_cd === fin_prdt_cd.value)
+    if (selectedThing) { 
+      selectedDeposit.value = selectedThing
+    }
+  }
+  
   // 예금 정보 가져오기
   onMounted(() => {
     store.getDeposit()
@@ -48,14 +57,9 @@
   
   // 예금 가입 유무 확인
   const isAdded = function (optionId) {
-    for (let index = 0; index < store.financial_products.length; index++) {
-      if (optionId === store.financial_products[index].deposits) {
-        return true
-      } else {
-        return false
-      }
-    }
+    return store.financial_products.some(product => product.deposits === optionId)
   }
+
   
 </script>
 
