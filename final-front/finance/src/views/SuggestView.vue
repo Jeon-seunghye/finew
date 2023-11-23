29eb7438e2aba7346958642f0bfa79f1a2cdaee6
jaehunbb <sirika2547@naver.com>
2023-11-23 20:32:52 +0900

    dummy

--- final-front/finance/src/views/SuggestView.vue
+++ final-front/finance/src/views/SuggestView.vue
@@ -4,9 +4,29 @@
     </div>
 </template>
 
-<script setup>
 
+
+<script setup>
+  import { ref, onMounted } from 'vue';
+  import { useRoute, useRouter } from 'vue-router';
+  import axios from 'axios';
+  import { useArticleStore } from '@/stores/article';
+
+  const router = useRouter();
+  const route = useRoute();
+  const store = useArticleStore();
+
+  console.log(store.users)
+  console.log(store.users.nickname)
 </script>
+
+
+
+
+
+
+
+
 
 <style scoped>
 
